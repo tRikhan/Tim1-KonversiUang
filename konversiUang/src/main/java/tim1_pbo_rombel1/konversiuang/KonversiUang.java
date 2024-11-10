b22c0b3e79beff30bae370d0d1442e75d7b009c2
Adonis <tafrikhan.rizki.praditya@students.untidar.ac.id>
2024-11-10 10:32:36 +0700

    upload kerangka projek
    upload kerangka projek awal dengan library yang dibutuhkan

---
+++ konversiUang/src/main/java/tim1_pbo_rombel1/konversiuang/KonversiUang.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tim1_pbo_rombel1.konversiuang;
+
+/**
+ *
+ * @author pc
+ */
+public class KonversiUang {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
